9198c14a287860ce3e59e09eb8be924813bf1ac8
John Swartzentruber <jswartzen@users.noreply.github.com>
2015-02-15 21:52:15 -0500

    Add Test project

---
+++ JSTZNet_Test/JSTZNetTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JSTZNet_Test
+{
+    [TestClass]
+    public class JSTZNetTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
