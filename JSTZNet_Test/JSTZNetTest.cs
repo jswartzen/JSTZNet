9a27ecbe987a8bd75c167931b41f54c254e40928
John Swartzentruber <jswartzen@users.noreply.github.com>
2016-06-11 16:45:51 -0400

    Commit old uncommitted changes
    I have no idea what these are, but they're a year old.

--- JSTZNet_Test/JSTZNetTest.cs
+++ JSTZNet_Test/JSTZNetTest.cs
@@ -1,8 +1,20 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="John L. Swartzentruber" file="JSTZNetTest.cs">
+//   Copyright 2015 John L. Swartzentruber
+// </copyright>
+// <summary>
+//   Defines the JSTZNetTest type.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace JSTZNet_Test
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Test class for the JSTZNet program and related classes
+    /// </summary>
     [TestClass]
     public class JSTZNetTest
     {
